68232c13962be8cb4498212e0b750d1aa57ca110
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2022-12-17 01:02:58 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,4 @@
-print("Hello world!")
+local chest = peripheral.find("minecraft:chest")
+for slot, item in pairs(chest.list()) do
+  print(("%d x %s in slot %d"):format(item.count, item.name, slot))
+end
