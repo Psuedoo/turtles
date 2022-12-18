8adef72beb10de3dff94884c417f25dc6ef7fc5f
Psuedoo <forthewin720@gmail.com>
2022-12-18 02:44:44 -0500

    update rs-storage

--- rs-monitor.lua
+++ rs-monitor.lua
@@ -9,11 +9,4 @@ end
 
 local maxStorage = bridge.getMaxItemDiskStorage()
 
-local function drawBar(x, y, width, height, percent, color)
-	monitor.setBackgroundColor(color)
-	monitor.fill(x, y, width, height, " ")
-	monitor.setBackgroundColor(colors.black)
-	monitor.fill(x, y, math.floor(width * percent), height, " ")
-end
-
-drawBar(1, 1, 51, 1, 1, colors.white)
+monitor.write(maxStorage)
